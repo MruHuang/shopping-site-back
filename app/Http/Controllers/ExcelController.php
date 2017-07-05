5a120da02b134e161b047c379efc3cc50d315328
monkeyteacher <peter1180041@gmail.com>
2017-07-05 17:30:44 +0800

    新增報表介面

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\PrintReport\PrintReport;
 use Excel;
 
 class ExcelController extends Controller
 {
     //Excel文件导出功能 By Laravel学院
+    private $pr;
+
+    public function __construct(PrintReport $pr){
+        $this->pr = $pr;
+    }
+
     public function export(){
         $cellData = [
             ['学号','姓名','成绩'],
@@ -25,6 +32,45 @@ class ExcelController extends Controller
     }
 
     public function PostPrintReport(Request $Request){
-        return $Request->All();
+        //return $Request->All();
+        $report_type = $Request->input('report_type');
+        $date_start = $Request->input('date_start');
+        $date_end = $Request->input('date_end');
+        if($report_type == 'sales_Details'){
+            $data = $this->pr->SalesDetails();
+            $excel_title = '銷售明細';
+        }else if($report_type == 'daily_Shipments_Person'){
+            //一個日期
+            $data = $this->pr->DailyShipmentsPerson($date_start);
+            $excel_title = '每日出貨明細(人)';
+        }else if($report_type == 'daily_Shipments_Commodity'){
+            //一個日期
+            $data = $this->pr->DailyShipmentsCommodity($date_start);
+            $excel_title = '每日出貨明細(物)';
+        }else if($report_type == 'daily_Pay'){
+            //一個日期
+            $data = $this->pr->DailyPay($date_start);
+            $excel_title = '每日貨款收入統計';
+        }else if($report_type == 'single_Sales_Details'){
+            //兩個日期
+            $data = $this->pr->SingleSalesDetails($date_start,$date_end);
+            $excel_title = '單項商品銷售明細';
+        }else if($report_type == 'VIP'){
+            //兩個日期
+            $data = $this->pr->VIP($date_start,$date_end);
+            $excel_title = 'VIP客戶';
+        }else{
+            $data = $this->pr->MemberIntegral($date_start);
+            $excel_title = '會員紅利';
+        }
+        $this->ExcelExport($data,$excel_title);
+    }
+
+    public function ExcelExport($data,$excel_title){
+        Excel::create('藍星報表',function($excel) use ($data,$excel_title){
+            $excel->sheet($excel_title, function($sheet) use ($data){
+                $sheet->rows($data);
+            });
+        })->export('xls');
     }
 }
